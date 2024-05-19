85069c7443450b48dcafee761917b1f2b10d77bc
divyanshu9821 <singhaldivyanshu2001@gmail.com>
2024-05-19 19:49:14 +0530

    Stop watch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import StopWatch from './stopWatch/stopWatch.jsx'
+function App(){
+  return (
+    <>
+      <StopWatch/>
+      <StopWatch/>
+      <StopWatch/>
+    </>
+  )
+}
+export default App;
